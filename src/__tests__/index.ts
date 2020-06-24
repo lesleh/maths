b02a6f1081cab25b1ffe2e3ca7e11b0f6661b622
Leslie Hoare <iam@lesleh.co.uk>
2020-06-24 15:15:30 +0100

    Fix function name

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -12,10 +12,10 @@ describe("square()", () => {
 
 describe("cube()", () => {
   it("correctly calculates the cube of 4", () => {
-    expect(square(4)).toBe(64);
+    expect(cube(4)).toBe(64);
   });
 
   it("correctly calculates the square of -10", () => {
-    expect(square(-10)).toBe(-1000);
+    expect(cube(-10)).toBe(-1000);
   });
 });
